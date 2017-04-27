5688d721308e1a5ea8baa91ed5005393eaa89615
Григорий Ким <kim-g@yandex.ru>
2017-04-27 22:44:29 +0500

    Заархивировал ненужные файлы

--- Прочие проекты/project/weather-station/server/php-sql/make_db.sql
+++
@@ -1,9 +0,0 @@
-
--- см. также config.php
-
--- создать БД
-CREATE DATABASE IF NOT EXISTS db_weather;
-
--- создать пользователя
-CREATE USER  'u_weather'@'localhost' IDENTIFIED BY '***PASSWORD***';
-GRANT ALL ON db_weather.* TO 'u_weather'@'localhost';
